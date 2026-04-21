3042a0c571fa594a86e3404ca3b343d019cf147a
reubenks <reubenks444@gmail.com>
2026-04-21 10:51:34 +0100

    added search and filter functionality to all cards page

--- src/app/pages/all-cards/all-cards.component.ts
+++ src/app/pages/all-cards/all-cards.component.ts
@@ -10,7 +10,45 @@ import { WalletService } from 'src/app/core/services/wallet.service';
 })
 export class AllCardsComponent implements OnInit {
 
+  //search and filter 
+  Search: string = ''
+  Sort: string = 'most new'
+  Energy: string = 'all'
+  Rarity: string = 'all'
+
   constructor(private router: Router, public wallet: WalletService, private spinner: NgxSpinnerService,) { }
+
+  // adding filter method
+
+  filteredCards() {
+    let filtered = this.wallet.allNfts
+
+    // filter by the pokemon name
+    if (this.Search != '') {
+      filtered = filtered.filter(card => card.name.toLocaleLowerCase().includes(this.Search.toLocaleLowerCase()))
+    }
+
+    // filter by the type of energy
+    if (this.Energy !='all') {
+      filtered = filtered.filter(card => card.energy_type == this.Energy)
+    }
+
+    // filter by the type of card rarity
+    if (this.Rarity != 'all') {
+      filtered = filtered.filter(card => card.rarity == this.Rarity)
+    }
+
+    // sort csrds by highest and lowest price
+    if (this.Sort == 'highest_price') {
+      filtered.sort((a, b) => Number(b.price) - Number(a.price))
+    }
+
+    if (this.Sort == 'lowest_price') {
+      filtered.sort((a, b) => Number(a.price) - Number(b.price))
+    }
+
+    return filtered
+  }
 
   ngOnInit(): void {
     if (this.wallet.allNfts.length == 0) {
